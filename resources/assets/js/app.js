ea5488c57ae539a72c5962c13748250b49ea6e42
snipe <snipe@snipe.net>
2017-03-14 19:23:35 -0700

    Fonts and JS

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,8 +13,53 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+/**
+ * Next, we will create a fresh Vue application instance and attach it to the
+ * page. Then, you may begin adding components to this application or customize
+ * the JavaScript scaffolding to fit your unique needs.
+ */
 
 const app = new Vue({
-    el: '#app'
+    el: '#vue-wrapper',
+
+    data: {
+        items: [],
+        hasError: true,
+        hasDeleted: true,
+        newItem : {'name':''}
+    },
+    mounted : function(){
+        this.getVueItems();
+    },
+    methods : {
+        getVueItems: function(){
+            axios.get('/vueitems').then(response => {
+                this.items = response.data;
+
+        });
+        },
+        createItem: function(){
+            var input = this.newItem;
+            if(input['name'] == ''){
+                this.hasError = false;
+                this.hasDeleted = true;
+            }
+            else{
+                this.hasError = true;
+                axios.post('/vueitems',input)
+                    .then(response => {
+                    this.newItem = {'name':''};
+                this.getVueItems();
+            });
+                this.hasDeleted = true;
+            }
+        },
+        deleteItem: function(item){
+            axios.post('/vueitems/'+item.id).then((response) => {
+                this.getVueItems();
+            this.hasError = true,
+                this.hasDeleted = false
+        });
+        },
+    }
 });
