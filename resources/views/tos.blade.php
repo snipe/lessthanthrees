bbcfe852bcb1075d182c0cfcbe8c6ff14d5d6383
snipe <snipe@snipe.net>
2017-03-15 20:18:48 -0700

    TOS/privacy

---
+++ resources/views/tos.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+
+                <div id="vue-wrapper">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
