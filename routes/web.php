ddd0f6a9a7e73ee6717ae071b3124b42fad5afa5
snipe <snipe@snipe.net>
2017-03-15 20:20:35 -0700

    Social login routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,26 @@ Route::get ( '/vueitems', 'HomeController@readItems' );
 Route::post ( '/vueitems', 'HomeController@storeItem' );
 Route::post ( '/vueitems/{id}', 'HomeController@deleteItem' );
 
+Route::get('auth/{provider}', [ 'as' => 'oauth', 'uses' => 'Auth\LoginController@redirectToProvider' ]);
+Route::get('auth/{provider}/callback', [ 'as' => 'oauth.callback', 'uses' => 'Auth\LoginController@handleProviderCallback' ]);
+
+
 /*
  * General routes
  */
+Route::get('/tos', function () {
+    return view('welcome');
+});
+
+Route::get('/privacy', function () {
+    return view('welcome');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 
 Auth::routes();
 
