6c200ca25c89ff512b2b2deab9b57350d1f219c3
snipe <snipe@snipe.net>
2017-03-15 20:30:34 -0700

    Added missing use for DB

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Cashier\Billable;
+use DB;
 
 class User extends Authenticatable
 {
