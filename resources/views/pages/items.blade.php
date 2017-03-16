b53babc8ab180f0b5a0566d98e7f66c6afb51771
snipe <snipe@snipe.net>
2017-03-16 03:45:52 -0700

    User page blades

---
+++ resources/views/pages/items.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+
+                @if (count($items) > 0)
+                <div class="table table-borderless" id="table">
+                    <table class="table table-borderless" id="table">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Notes</th>
+                        </tr>
+                        </thead>
+                        @foreach ($items as $item)
+                        <tr>
+                            <td><i class="{{ $item->category->icon }}"></i> {{ $item->name }}</td>
+                            <td>{{ $item->description }}</td>
+                        </tr>
+                        @endforeach
+                    </table>
+                @else
+                        <p>Nothing here yet!</p>
+                @endif
+
+                <div id="vue-wrapper">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
