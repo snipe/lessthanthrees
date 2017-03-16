69c5944baceeabd17d8985eb8d0d55bdff9aaf38
snipe <snipe@snipe.net>
2017-03-16 13:17:57 -0700

    Fave item controller methods

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Item;
+use App\Fave;
 use Auth;
 use App\Category;
+use DB;
 
 class ItemsController extends Controller
 {
@@ -49,23 +51,56 @@ class ItemsController extends Controller
         Item::find($request->id)->where('user_id','=',Auth::user()->id)->delete();
     }
 
+    public function faveItem(Request $request) {
+        if ($item = Item::find($request->id)->get()) {
+            return Fave::firstOrCreate(
+                [
+                    'item_id' => $request->id,
+                    'user_id' => Auth::user()->id
+                ]);
+        }
+    }
+
+    public function unfaveItem(Request $request) {
+        if ($fave = Fave::find($request->id)) {
+             $fave->delete();
+                 return true;
+        }
+        return false;
+    }
+
 
     public function showUserHome() {
         return view('welcome');
     }
 
-    public function showUserItems(Request $request, $subdomain, $category = null) {
+    public function showItemsPage(Request $request, $subdomain, $category = null) {
 
-        $category = Category::where('slug','=',$category)->first();
-
-        if ($category) {
-            $items = Item::with('category')
-                ->where('category_id','=',$category->id)
-                ->where('user_id','=',$request->selected_account->id)->get();
-            return view('pages/items')->with('items',$items)->with('category',$category);
+        if ($category = Category::where('slug','=',$category)->first()) {
+            return view('pages/items')->with('category',$category);
         }
 
-        return view('pages/home')->with('error','Invalid category');
+        return view('pages/items')->with('error','Invalid category');
 
     }
+
+    public function showUserItems(Request $request, $subdomain, $category = null) {
+
+        if ($category = Category::where('slug','=',$category)->first()) {
+            return  Item::with('category')
+                ->where('category_id','=',$category->id)
+                ->where('user_id','=',$request->selected_account->id)->get();
+        }
+
+    }
+
+    public function showUserFaves() {
+        return Auth::user()->faves;
+    }
+
+    public function showUserFavesPage() {
+            return view('faves');
+    }
+
+
 }
