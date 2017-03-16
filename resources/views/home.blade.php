474a7a8709460c07eb9bed9ee29e940e6ad587fb
snipe <snipe@snipe.net>
2017-03-15 23:27:14 -0700

    Display updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,43 +3,60 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
 
 
             <div id="vue-wrapper">
                 <div class="content">
                     <div class="form-group row">
-                        <div class="col-md-8">
+                        <div class="col-md-4">
                             <input type="text" class="form-control" id="name" name="name"
-                                   required v-model="newItem.name" placeholder=" Enter some name">
+                                   required v-model="newItem.name" placeholder="Name of a thing you love">
 
                         </div>
                         <div class="col-md-4">
-                            <button class="btn btn-primary" @click.prevent="createItem()"
-                                    id="name" name="name">
+                            <input type="text" class="form-control" id="description" name="description"
+                                   required v-model="newItem.description" placeholder="Why do you love it?">
+
+                        </div>
+                        <div class="col-md-2">
+                            <select name="category_id" class="form-control" v-model="newItem.category_id">
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+
+                        </div>
+                        <div class="col-md-1">
+                            <button class="btn btn-primary" @click.prevent="createItem()" id="name" name="name">
                                 <span class="glyphicon glyphicon-plus"></span> ADD
                             </button>
                         </div>
                     </div>
                     <p class="text-center alert alert-danger"
-                       v-bind:class="{ hidden: hasError }">Please enter some value!</p>
+                       v-bind:class="{ hidden: hasError }">Please enter a value!</p>
                     {{ csrf_field() }}
                     <p class="text-center alert alert-success"
                        v-bind:class="{ hidden: hasDeleted }">Deleted Successfully!</p>
+
                     <div class="table table-borderless" id="table">
                         <table class="table table-borderless" id="table">
                             <thead>
                             <tr>
-                                <th>ID</th>
                                 <th>Name</th>
+                                <th>Category</th>
+                                <th>Notes</th>
                                 <th>Actions</th>
                             </tr>
                             </thead>
                             <tr v-for="item in items">
-                                <td>@{{ item.id }}</td>
                                 <td>@{{ item.name }}</td>
-                                <td @click.prevent="deleteItem(item)" class="btn btn-danger"><span
-                                            class="glyphicon glyphicon-trash"></span></td>
+                                <td>@{{ item.category.name }}</td>
+                                <td>@{{ item.description }}</td>
+                                <td>
+                                    <a @click.prevent="deleteItem(item)" class="btn btn-danger btn-sm">
+                                        <span class="glyphicon glyphicon-trash"></span></a>
+                                </td>
                             </tr>
                         </table>
                     </div>
