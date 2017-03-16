757ffeaa2a457fea2642fbf11f9021a965535679
snipe <snipe@snipe.net>
2017-03-16 03:44:32 -0700

    Updated JS

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -73,6 +73,7 @@ $(function() {
     $('#login-form-link').click(function(e) {
         $("#login-form").delay(100).fadeIn(100);
         $("#register-form").fadeOut(100);
+        $("#register-form").hide();
         $('#register-form-link').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
@@ -80,6 +81,7 @@ $(function() {
     $('#register-form-link').click(function(e) {
         $("#register-form").delay(100).fadeIn(100);
         $("#login-form").fadeOut(100);
+        $("#login-form").hide();
         $('#login-form-link').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,7 +9,8 @@ window._ = require('lodash');
 
 window.$ = window.jQuery = require('jquery');
 
-require('bootstrap-sass');
+require('bootstrap-less');
+require('fontawesome');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
