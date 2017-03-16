da09e83e05523228f6e8f604b197a99964002c62
snipe <snipe@snipe.net>
2017-03-16 13:20:37 -0700

    Switch to vue loop for item display

--- resources/views/pages/items.blade.php
+++ resources/views/pages/items.blade.php
@@ -4,28 +4,29 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <div id="vue-wrapper">
 
-                @if (count($items) > 0)
                 <div class="table table-borderless" id="table">
                     <table class="table table-borderless" id="table">
                         <thead>
                         <tr>
                             <th class="col-md-4">Name</th>
-                            <th class="col-md-8">Notes</th>
+                            <th class="col-md-7">Notes</th>
+                            <th class="col-md-1"></th>
                         </tr>
                         </thead>
-                        @foreach ($items as $item)
-                        <tr>
-                            <td><i class="{{ $item->category->icon }}"></i> {{ $item->name }}</td>
-                            <td>{{ $item->description }}</td>
-                        </tr>
-                        @endforeach
-                    </table>
-                @else
-                        <p>Nothing here yet!</p>
-                @endif
 
-                <div id="vue-wrapper">
+                        <tr v-for="item in items">
+                            <td>@{{ item.name }}</td>
+                            <td>@{{ item.description }}</td>
+                            <td>
+                                <a @click.prevent="faveItem(item)" class="heart-grey">
+                                    <span class="glyphicon glyphicon-heart"></span></a>
+                            </td>
+                        </tr>
+                    </table>
+
+
                 </div>
             </div>
         </div>
