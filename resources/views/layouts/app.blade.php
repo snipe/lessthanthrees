742ab02d3e533f1c58fd504d6a10b7e177ed5ac0
snipe <snipe@snipe.net>
2017-03-15 20:20:50 -0700

    Added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" type="image/png" href="{{ url('/') }}">
+
     <title>{{ config('app.name') }}</title>
 
     <!-- Styles -->
