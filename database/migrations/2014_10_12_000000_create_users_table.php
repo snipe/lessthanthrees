981cad62c4b8b1fcac0d51ab6f2364e9853a6dc3
snipe <snipe@snipe.net>
2017-03-15 20:20:05 -0700

    New migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,33 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('email')->nullable()->default(null);
             $table->string('password');
+            $table->string('provider');
+            $table->string('provider_id')->unique();
             $table->rememberToken();
             $table->timestamps();
+        });
+
+        /*
+      * Create social login table
+      */
+        Schema::create('social', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('service');
+            $table->string('uid')->nullable()->default(NULL);
+            $table->timestamps();
+            $table->string('access_token')->default(NULL)->nullable()->default(NULL);
+            $table->integer('end_of_life')->default(NULL)->nullable()->default(NULL);
+            $table->string('refresh_token')->default(NULL)->nullable()->default(NULL);
+            $table->string('request_token')->default(NULL)->nullable()->default(NULL);
+            $table->string('request_token_secret')->default(NULL)->nullable()->default(NULL);
+            $table->text('extra_params')->default(NULL)->nullable()->default(NULL);
+            $table->string('access_token_secret')->default(NULL)->nullable()->default(NULL);
         });
     }
 
@@ -31,5 +54,6 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('social');
     }
 }
---
+++ database/migrations/2017_03_15_163745_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function ($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description')->nullable()->default(null);
+            $table->integer('parent_id')->nullable()->default(null);
+            $table->timestamps();
+        });
+
+        Schema::create('user_likes', function ($table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('fave_id')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
