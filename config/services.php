a56766ce7599fe0e5c808193bd7f3407fcfa2f23
snipe <snipe@snipe.net>
2017-03-18 17:45:47 -0700

    Updated stripe env variable

--- config/services.php
+++ config/services.php
@@ -31,8 +31,8 @@ return [
 
     'stripe' => [
         'model'  => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+        'key' => env('STRIPE_PUBLIC_KEY'),
+        'secret' => env('STRIPE_PRIVATE_KEY'),
     ],
 
     'github' => [
